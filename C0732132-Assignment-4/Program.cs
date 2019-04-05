62069ee409256f1230220640c74911a30ef086da
visualstudio <visualstudio@122-PC37>
2019-04-05 18:53:59 -0400

    Section A

--- C0732132-Assignment-4/Program.cs
+++ C0732132-Assignment-4/Program.cs
@@ -15,46 +15,55 @@ namespace c0732132_Assihnment_4
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
+            p.ReadTextFiles();
+            Console.ReadKey();
 
         }
-        public void Run() { this.ReadTextFiles(); }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
 
         public void ReadTextFiles()
         {
             // Read file using StramReader. Read file line by line
             using (StreamReader file = new StreamReader("U:/Users/732132/c0732132-Assignment4/C0732132-Assignment-4/beowulf.txt"))
             {
+
                 int counter = 0;
                 string ln;
 
                 while ((ln = file.ReadLine()) != null)
                 {
-                    Console.WriteLine(ln);
+                    
                     Beowulf.Add(ln);
-
+                  
                 }
+
                 file.Close();
+                counter = File.ReadLines("U:/Users/732132/c0732132-Assignment4/C0732132-Assignment-4/beowulf.txt").Count();
                 Console.WriteLine($"File has {counter} lines.");
+                
             }
         }
+
         public int FindNumberOfBlankSpaces(string line)
         {
+           
             int countletters = 0;
             int countSpaces = 0;
 
             foreach (char c in line)
             {
-                if (char.IsLetter(c))
-                {
-                    countletters++;
-                }
-                if (char.IsWhiteSpace(c))
-                {
-                    countSpaces++;
-                }
-                return countSpaces;
+                if (char.IsLetter(c)) { countletters++; }
+
+                if (char.IsWhiteSpace(c)) { countletters++; }
             }
+            return countSpaces;
+
         }
+
     }
+    
 }
 
